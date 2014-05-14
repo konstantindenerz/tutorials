61e4a22463e8443962e7440b282ff0169a707209
konstantindenerz <konstantin.denerz+github@googlemail.com>
2014-05-14 22:32:27 +0200

    added source code documentation

--- mongodb.crud/db-config.js
+++ mongodb.crud/db-config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  /* The connection string with a host, a port and the db name. */
   url: 'mongodb://localhost:27017/crud',
+  /* the name of document collection that should be used by DAO. */
   collection: 'users'
 };
